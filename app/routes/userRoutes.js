37d91d4acd93a1acd41e413d8c6b6624d0625058
lesmanjarquin <jarquinlesman2002@gmail.com>
2024-11-16 23:17:11 -0600

    feat: se agregó el endpoint para cambiar contraseña

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -7,6 +7,7 @@ const apiRoutes = express.Router();
 
 apiRoutes.post('/signUp', async (req, res) => await usuarioController.signUp(req, res))
     .post('/login', async (req, res) => await usuarioController.signIn(req, res))
-    .get('/whoAmI', auth.whoAmI, async (req, res) => await usuarioController.getUserInfo(req, res));
+    .get('/whoAmI', auth.whoAmI, async (req, res) => await usuarioController.getUserInfo(req, res))
+    .put('/resetPassword', async(req, res) => await usuarioController.resetPassword(req, res));
 
 module.exports = apiRoutes;
