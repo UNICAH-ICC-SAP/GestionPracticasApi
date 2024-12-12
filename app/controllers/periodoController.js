530364e8b6590cd220384e7648e0207507f11b73
Kevin504Hn <kevinbarahona616@gmail.com>
2024-12-12 16:31:23 -0600

    Se crearon los siguientes archivos Controlador,Modelo y Ruta de Periodo

---
+++ app/controllers/periodoController.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const db = require('../config/db')
+const Periodo = db.periodos;
+module.exports = {
+    insert,
+    get
+}
+
+async function insert(req, res) {
+    const periodo = req.body;
+    Periodo.create({
+        id_periodo: periodo.id_periodo,
+        fecha_inicio: periodo.fecha_inicio,
+        fecha_final: periodo.fecha_final
+
+    }).then(data => {
+        res.status(200).send(data);
+    }).catch(error => {
+        console.error(error)
+        res.status(500).send({
+            message:
+                error.message || "Sucedio un error al insertar el registro de Periodo"
+        });
+    });
+}
+
+async function get(req, res) {
+    Periodo.findAll() 
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Sucedió un error al obtener los registros de Pensum"
+            });
+        });
+}
