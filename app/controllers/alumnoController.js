bd086788b306838f826a7d40e6466d19e746d339
jodanielrm <126302647+jodanielrm@users.noreply.github.com>
2024-12-12 00:15:09 -0600

    fix:agrego actualizar alumnos (#18)

--- app/controllers/alumnoController.js
+++ app/controllers/alumnoController.js
@@ -9,7 +9,9 @@ module.exports = {
     findAll,
     findBy,
     insert,
-    findAllData
+    findAllData,
+    update,
+    updateStatus
 }
 
 async function insert(req, res) {
@@ -60,13 +62,14 @@ async function findAllData(req, res) {
 }
 
 async function findBy(req, res) {
-    Alumno.findAll({ where: req.query })
+    Alumno.findAll({ where: req.query, include: [{ model: Facultad }]})
+    
         .then(data => {
             const newData = {
                 userId: data[0].alumnoId,
                 email: data[0].email,
                 nombre: data[0].nombre,
-                facultadId: data[0].facultadId,
+                nombreFacultad: data[0].nombreFacultad,
                 telefono: data[0].telefono
             }
             res.status(201).send(newData);
@@ -76,4 +79,64 @@ async function findBy(req, res) {
                 message: err.message || "Sucedio un error al obtener los registros de Alumnoes"
             })
         })
+}
+
+async function update(req, res) {
+    const { alumnoId } = req.query;
+    const updatedData = req.body;
+
+    if (!alumnoId) {
+        return res.status(400).send({
+            message: "El Id del Alumno es necesario para la actualización."
+        });
+    }
+
+    Alumno.update(updatedData, {
+        where: { alumnoId: alumnoId }
+    }).then(num => {
+        if (num == 1) {
+            res.status(200).send({
+                message: "Alumno actualizado correctamente."
+            });
+        } else {
+            res.status(404).send({
+                message: `No se pudo encontrar ni actualizar el alumno con ID=${alumnoId}`
+            });
+        }
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Sucedió un error al actualizar el alumno."
+        });
+    });
+}
+
+async function updateStatus(req, res) {
+    const alumnoId = req.query.alumnoId;
+
+    if (!alumnoId) {
+        return res.status(400).send({
+            message: "El Id del alumno es necesario para poder desactivar al alumno."
+        });
+    }
+
+    try {
+        const [num] = await Alumno.update(
+            { estadoAlumno: 0 },
+            { where: { alumnoId: alumnoId } }
+        );
+
+        if (num === 1) {
+            res.status(200).send({
+                message: "Alumno desactivado correctamente."
+            });
+        } else {
+            res.status(404).send({
+                message:`No se pudo encontrar ni desactivar el alumno con ID=${alumnoId}`
+            });
+        }
+    } catch (error) {
+        res.status(500).send({
+            message: error.message || "Sucedió un error al desactivar el alumno."
+   });
+}
 }
