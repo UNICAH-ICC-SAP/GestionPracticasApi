afdd87144772c64ef5393cdb6b2ebaebe5d249b3
lesmanjarquin <jarquinlesman2002@gmail.com>
2024-12-12 18:14:29 -0600

    feat: se agregó el controller para insertar, actualizar y eliminar secciones

--- app/routes/seccionesRoutes.js
+++ app/routes/seccionesRoutes.js
@@ -5,6 +5,8 @@ const seccionesController = require('../controllers/seccionesController');
 const auth = require('../middlewares/auth')
 const apiRoutes = express.Router();
 
-apiRoutes.post('/insertSection', auth.isAuth, async (req, res) => await seccionesController.insert(req, res));
+apiRoutes.post('/insertSection', auth.isAuth, async (req, res) => await seccionesController.insert(req, res))
+        .put('/updateSection', auth.isAuth, async (req, res) => await seccionesController.update(req, res))
+        .delete('/deleteSection', auth.isAuth, async (req, res) => await seccionesController.deleteSection(req, res));
 
 module.exports = apiRoutes;
