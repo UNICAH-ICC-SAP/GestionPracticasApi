fe2103a57c76812147e510929510d1f86c5a6738
Ariel Calix <acalixv@unicah.edu>
2025-01-06 11:37:36 -0600

    fix: deploy

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const db = require('./app/config/db');
 const App = require('./app/app');
 require('dotenv').config()
 
-db.sequelizeInstance.sync().then(() => {
-    console.info("Synced");
-})
 /*eslint-disable no-undef*/
-App.listen(parseInt(process.env.APP_PORT), function (error) {
-    if (error) return console.error(error);
-    console.info(`server running on port: ${process.env.APP_PORT}`);
-});
+const PORT = process.env.PORT || process.env.APP_PORT;
 /*eslint-enable no-undef*/
+
+db.sequelizeInstance.sync()
+    .then(() => {
+        console.info("Synced");
+        App.listen(parseInt(PORT), function (error) {
+            if (error) return console.error(error);
+            console.info(`server running on port: ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error("Failed to sync database:", error);
+    })
