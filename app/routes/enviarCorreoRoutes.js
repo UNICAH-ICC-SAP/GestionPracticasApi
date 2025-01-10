cae25a644aa5138e8e64e0bef12a0c0201880a65
Acalix-unicah <acalixv@unicah.edu>
2025-01-10 13:48:11 -0600

    fix: envio de correo (#43)

--- app/routes/enviarCorreoRoutes.js
+++ app/routes/enviarCorreoRoutes.js
@@ -11,17 +11,7 @@ correoRoutes.get('/obtenerPlantillas', auth.isAuth, async (req, res) => await co
     .put('/actualizarPlantilla/:correoId', auth.isAuth, async (req, res) => await correoController.update(req, res))
     .put('/cambiarEstadoPlantilla/:correoId', auth.isAuth, async (req, res) => await correoController.updateStatus(req, res))
     .post('/enviarCorreo/:correoId', auth.isAuth, async (req, res) => {
-        const { correoId } = req.params;  // El correoId viene desde los parámetros de la ruta
-        const { correoDestino, userId, nombreUsuario } = req.body; // Los demás datos vienen en el body
-    
-        // Validar que todos los parámetros necesarios están presentes
-        if (!correoId || !correoDestino || !userId || !nombreUsuario) {
-            return res.status(400).json({ message: 'Faltan parámetros en el cuerpo de la solicitud.' });
-        }
-    
-        req.params = { correoId, userId, nombreUsuario, correoDestino };
-    
-        await correoController.enviarCorreo(req, res, userId, nombreUsuario, correoDestino);
+        await correoController.enviarCorreo(req, res);
     });
 
 module.exports = correoRoutes;
